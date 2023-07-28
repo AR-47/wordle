e7ac30d88b4fab8412adcd04d8838ab2477b3f9a
Adil Rahman <arahman.99@outlook.com>
2023-07-28 01:32:07 +0100

    updated countOccurrences to add keys as lowercase chars

--- src/helpers/countOccurrences.ts
+++ src/helpers/countOccurrences.ts
@@ -1,16 +1,15 @@
 import occurrencesInterface from "../interfaces/occurrencesInterface";
 
 function countOccurrences(str: string): occurrencesInterface {
-    const occurrences: occurrencesInterface = {};
-    for (const char of str) {
-      if (occurrences[char.toUpperCase()] !== undefined) {
-        occurrences[char.toUpperCase()] += 1;
-      } else {
-        occurrences[char.toUpperCase()] = 1;
-      }
+  const occurrences: occurrencesInterface = {};
+  for (const char of str) {
+    if (occurrences[char.toLowerCase()] !== undefined) {
+      occurrences[char.toLowerCase()] += 1;
+    } else {
+      occurrences[char.toLowerCase()] = 1;
     }
-    return occurrences;
+  }
+  return occurrences;
 }
 
 export default countOccurrences;
-  

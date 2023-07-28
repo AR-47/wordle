7c75155317e7332a8412b7f790c64b54cb09d23b
Adil Rahman <arahman.99@outlook.com>
2023-07-28 01:32:44 +0100

    updated pseudocode and started new implementation

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,39 @@
  * 
  * function name: markWordleGuess
  * input: guess(string) and hiddenTarget(string)
- * return something of the type below
+ * return a marking of the guess of the type below
  * interface MarkedGuess {
-    inCorrectLocation: string[],
-    inWord: string[],
-    notInWord: string[]
+    inCorrectLocation: (string | number)[][],
+    inWord: (string | number)[][],
+    notInWord: (string | number)[][]
 }
  * 
  * example 1: hiddenTarget is 'train'
  * a guess of 'ramps' should return
- * 
- * example 2: hiddenTarget is 'party'
- * a guess of 'apple': what should happen
- * 1. a is not in the correct location but is in word
- * 2. the first p is not in the correct location but is in the word
- * 3. 
- * 
  * {
  *  inCorrectLocation: [],
-    inWord:['r', 'a'],
-    notInWord: ['m', 'p', 's']
+    inWord:[ ['r', 0], ['a', 1] ],
+    notInWord: [ ['m', 2], ['p', 3], ['s', 4] ]
+ * }
+ * 
+ * example 2: hiddenTarget is 'party'
+ * a guess of 'apple' should return
+ * {
+ *  inCorrectLocation: [],
+    inWord:[ ['a', 0], ['p', 1] ],
+    notInWord: [ ['r', 2], ['t', 3], ['y'], 4 ]
  * }
  * 
  * pseudocode:
  * 
- * create a new variable called occurrencesInTarget = {actual occurences} ***helper function
+ * create a new variable called occurrencesInTarget and set it to {actual occurences} ***HELPER FUNCTION
  * create a new variable called correctOccurrencesInGuess = {}
  * create new variable called markedGuess = {}
  * for each letter in guess
- *  if correctOccurrencesInGuess.letter < occurrencesInTarget.letter AND letter is in the correct location*** helper function
+ *  if correctOccurrencesInGuess.letter (need to deal with undefined values) < occurrencesInTarget.letter AND letter is in the correct location*** HELPER FUNCTION
  *      then add letter to markedGuess.inCorrectLocation
  *      and add letter occurrence to correctOccurrencesInGuess
- *  else correctOccurrencesInGuess.letter < occurrencesInTarget.letter AND if letter is in the word*** helper function
+ *  else if correctOccurrencesInGuess.letter (need to deal with undefined values) < occurrencesInTarget.letter AND if letter is in the word*** HELPER FUNCTION
  *      then add letter to markedGuess.inWord
  *      and add letter occurrence to correctOccurrencesInGuess
  *  else
@@ -41,8 +42,67 @@
  *  
  */
 
-interface MarkedGuess {
-    inCorrectLocation: string[],
-    inWord: string[],
-    notInWord: string[]
+import { Console } from "console";
+import countOccurrences from "./helpers/countOccurrences";
+import isLetterInSamePosition from "./helpers/isLetterInSamePosition";
+import isLetterInWord from "./helpers/isLetterInWord";
+import MarkedGuess from "./interfaces/markedGuess";
+import occurrencesInterface from "./interfaces/occurrencesInterface";
+
+function markWordleGuess(guess: string, hiddenTarget: string): MarkedGuess {
+  const occurrencesInTarget = countOccurrences(hiddenTarget);
+  const correctOccurrencesInGuess: occurrencesInterface = {};
+
+  const lowerCaseGuess = guess.toLowerCase();
+  const lowerCaseHiddenTarget = hiddenTarget.toLowerCase();
+
+  lowerCaseGuess.split("").forEach((letter) => {
+    correctOccurrencesInGuess[letter] = 0;
+  });
+
+  const markedGuess: MarkedGuess = {
+    inCorrectLocation: [],
+    inWord: [],
+    notInWord: [],
+  };
+
+  const guessLettersArray = guess.split("").entries();
+
+  console.log(
+    "BUG TESTING: ",
+    correctOccurrencesInGuess["l"] < occurrencesInTarget["l"],
+    isLetterInSamePosition(0, "l", hiddenTarget)
+  );
+
+  for (const [index, letter] of guessLettersArray) {
+    console.log(
+      `Have we found all occurrences of ${letter}: ${
+        correctOccurrencesInGuess.letter < occurrencesInTarget.letter
+      }`
+    );
+    if (
+      isLetterInSamePosition(index, letter, hiddenTarget) &&
+      correctOccurrencesInGuess.letter < occurrencesInTarget.letter
+    ) {
+      markedGuess.inCorrectLocation.push([letter, index]);
+      correctOccurrencesInGuess.letter += 1;
+    } else if (
+      isLetterInWord(letter, hiddenTarget) &&
+      correctOccurrencesInGuess.letter < occurrencesInTarget.letter
+    ) {
+      console.log(
+        `correctOccurrencesInGuess of letter: ${correctOccurrencesInGuess.letter}`
+      );
+      markedGuess.inWord.push([letter, index]);
+      correctOccurrencesInGuess.letter += 1;
+    } else {
+      markedGuess.notInWord.push([letter, index]);
+    }
+  }
+
+  return markedGuess;
 }
+
+export default markWordleGuess;
+
+console.log(markWordleGuess("lulls", "level"));
